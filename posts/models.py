aca7afeb02548e5d79b70549a3e3f3ab3768899f
jean.angelis <jean.angelis@gmail.com>
2016-05-29 20:20:01 -0300

    Post class created

--- posts/models.py
+++ posts/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField()
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
