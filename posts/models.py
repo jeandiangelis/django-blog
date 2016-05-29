bad167562b9a8293c484811d23e83f1b222d9714
jean.angelis <jean.angelis@gmail.com>
2016-05-29 20:21:56 -0300

    Post migrations created

--- posts/models.py
+++ posts/models.py
@@ -6,10 +6,10 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=120)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-    timestamp = models.DateTimeField(auto_now=True, auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return self.title
