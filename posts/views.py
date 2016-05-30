65b0acab77984de18d54b1420e60185c23ef8b5c
jean.angelis <jean.angelis@gmail.com>
2016-05-30 09:12:14 -0300

    detail view for post created

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
 
 
 def post_detail(request):
+    post = get_object_or_404(Post, id=1)
     context = {
-        "title": "detail"
+        "title": "detail",
+        "post": post
     }
 
     return render(request, "posts/detail.html", context)
