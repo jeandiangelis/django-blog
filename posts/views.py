2c6c70957dcb57bfad85008b17ee293a49d69c81
jean.angelis <jean.angelis@gmail.com>
2016-05-30 08:08:00 -0300

    Url file for posts created

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def post_home(request):
+
+    return HttpResponse("<h1>Hello world!</h1>")
