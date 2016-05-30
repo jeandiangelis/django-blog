50003f982fcf8fa88f74fb2dc83be85dd01a49c0
jean.angelis <jean.angelis@gmail.com>
2016-05-30 08:14:10 -0300

    CRUD controller created for posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(r'^$', 'posts.views.post_home'),
+    url(r'^$', 'posts.views.post_list'),
+    url(r'^create/$', 'posts.views.post_create'),
+    url(r'^detail/$', 'posts.views.post_detail'),
+    url(r'^delete/$', 'posts.views.post_delete'),
+    url(r'^update/$', 'posts.views.post_update'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,26 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def post_home(request):
+def post_detail(request):
 
     return HttpResponse("<h1>Hello world!</h1>")
+
+
+def post_create(request):
+
+    return HttpResponse("")
+
+
+def post_list(request):
+
+    return HttpResponse("")
+
+
+def post_update(request):
+
+    return HttpResponse("")
+
+
+def post_delete(request):
+
+    return HttpResponse("")
