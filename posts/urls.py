80ac6574a5f534959cdfabd8ca4c2d95cd680214
jean.angelis <jean.angelis@gmail.com>
2016-05-30 21:36:06 -0300

    Added dynamic routing on template

--- posts/urls.py
+++ posts/urls.py
@@ -10,7 +10,7 @@ from .views import (
 urlpatterns = [
     url(r'^$', post_list),
     url(r'^create/$', post_create),
-    url(r'^detail/(?P<id>\d+)/$', post_detail),
+    url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
     url(r'^delete/$', post_delete),
     url(r'^update/$', post_update),
 ]
