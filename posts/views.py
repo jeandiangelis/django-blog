3d156bdb1006328bac3e2c5ae3ca195e20f2b58c
jean.angelis <jean.angelis@gmail.com>
2016-05-31 12:56:46 -0300

    Form create created

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -16,8 +17,14 @@ def post_detail(request, id=None):
 
 
 def post_create(request):
+    form = PostForm()
+    if request.method == 'POST':
+        print request.POST.get('content')
+    context = {
+        "form": form,
+    }
 
-    return HttpResponse("")
+    return render(request, 'posts/form.html', context)
 
 
 def post_list(request):
