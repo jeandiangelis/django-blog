06921b90c5712bfed24b523f77c9b9bdc18925ab
jean.angelis <jean.angelis@gmail.com>
2016-06-07 21:18:51 -0300

    Added width and height

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,10 @@ from django.core.urlresolvers import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
-    image = models.FileField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True,
+                              width_field="width", height_field="height")
+    height = models.IntegerField(default=0)
+    width = models.IntegerField(default=0)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now_add=True)
